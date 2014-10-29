7ffc6b3bd0ed578f195be55592bb8bab9534cce3
itzmjauz <itzmjauz@gmail.com>
2014-10-29 20:42:42 +0100

    usage of readline

--- client.js
+++ client.js
@@ -1,47 +1,46 @@
-prompt = require('prompt')
+readl = require('readline')
 net   = require('net')
 HOST  = 'wolkje-69.cs.vu.nl'
 PORT  = 5378
 
+rl = readl.createInterface({
+  input : process.stdin,
+  output: process.stdout
+})
+
+function ask(){
+  rl.question("Message: ", function(answer){
+    rl.close()
+    client.write(answer+'\n')
+  })
+}
+
 client = new net.Socket()
 client.connect(PORT, HOST, function() {
-  connected = true
-
-  console.log('CONNECTED TO: ' + HOST + ':' + PORT)
-  prompt.start()
-  promptOn()
+  process.stdout.write('CONNECTED TO: ' + HOST + ':' + PORT+'\n')
+  ask()
 })
 
 client.on('data', function(data) {
   if(data=="BAD-RQST-HDR") {
-    console.log("Bad Header")
+    process.stdout.write("Bad Header")
     client.destroy()
   }
   if(data=="BAD-RQST-BODY"){
-    console.log("Bad Body")
+    process.stdout.write("Bad Body")
     client.destroy()
   }
   if(data=="IN-USE"){
-    console.log("Name already in use, please retry")
+    process.stdout.write("Name already in use, please retry")
     client.destroy()
   }
   else {
-    console.log(''+data)
-    promptOn()
+    process.stdout.write(''+data)
+    ask()
   }
 })
 
-function promptOn(){
-  if(connected==false) client.destroy()
-  else {
-    prompt.get(['msg'], function(err,result){
-      client.write(result.msg+'\n')
-      promptOn()
-    })
-  }
-}
 
 client.on('close', function() {
-  connected=false
   console.log('Connection closed')
 })
